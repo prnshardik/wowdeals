65a0807792845e36bbe946a66a7597ab3d6067d0
Gajjar Mitul <gajjarmitul007@gmail.com>
2021-02-20 00:46:08 +0530

    20210220 -0046

---
+++ resources/views/companies.blade.php
@@ -0,0 +1,185 @@
+@extends('layout.app')
+
+@section('title')
+Index
+@endsection
+
+@section('meta')
+@endsection
+
+@section('styles')
+@endsection
+
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"  id="mySmallModalLabel">Modal Title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+            </div>
+        </div>
+  </div>
+</div>
+@section('content')
+<div class="row gutters">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+
+        <div class="table-container">
+            <div class="table-responsive">
+                <table class="table custom-table m-0">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Company Name</th>
+                            <th>Company Icon</th>
+                            <th>Action</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Swiggy</td>
+                            <td><img src="img/icon.png" height="50px;" width="50px;"/></td>
+                            <td>
+                                
+                                <div class="td-actions">
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Edit Company">
+                                        <i class="icon-mode_edit"></i>
+                                    </a>
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Delete Company">
+                                        <i class="icon-trash-2"></i>
+                                    </a>
+
+                                </div>
+                                
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Swiggy</td>
+                            <td><img src="img/icon.png" height="50px;" width="50px;"/></td>
+                            <td>
+                                
+                                <div class="td-actions">
+
+                                    
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Edit Company">
+                                        <i class="icon-mode_edit"></i>
+                                    </a>
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Delete Company">
+                                        <i class="icon-trash-2"></i>
+                                    </a>
+
+                                </div>
+                                
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Swiggy</td>
+                            <td><img src="img/icon.png" height="50px;" width="50px;"/></td>
+                            <td>
+                                
+                                <div class="td-actions">
+
+                                    
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Edit Company">
+                                        <i class="icon-mode_edit"></i>
+                                    </a>
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Delete Company">
+                                        <i class="icon-trash-2"></i>
+                                    </a>
+
+                                </div>
+                                
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Swiggy</td>
+                            <td><img src="img/icon.png" height="50px;" width="50px;"/></td>
+                            <td>
+                                
+                                <div class="td-actions">
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Edit Company">
+                                        <i class="icon-mode_edit"></i>
+                                    </a>
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Delete Company">
+                                        <i class="icon-trash-2"></i>
+                                    </a>
+
+                                </div>
+                                
+                            </td>
+                            
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Swiggy</td>
+                            <td><img src="img/icon.png" height="50px;" width="50px;"/></td>
+                            <td>
+                                
+                                <div class="td-actions">
+                                
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Edit Company">
+                                        <i class="icon-mode_edit"></i>
+                                    </a>
+
+                                    <a href="#" class="icon red" data-toggle="tooltip" data-placement="top" title="Delete Company">
+                                        <i class="icon-trash-2"></i>
+                                    </a>
+
+                                </div>
+                                
+                            </td>
+                            
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+@endsection
+
+<div class="card-body">
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-end primary">
+            <li class="page-item disabled"><a class="page-link" href="#">
+            <i class="icon-chevron-left"></i></a></li>
+            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">4</a></li>
+            <li class="page-item"><a class="page-link" href="#">5</a></li>
+            <li class="page-item"><a class="page-link" href="#">6</a></li>
+            <li class="page-item"><a class="page-link" href="#">7</a></li>
+            <li class="page-item"><a class="page-link" href="#"><i class="icon-chevron-right"></i></a></li>
+        </ul>
+    </nav>
+</div>
+@section('scripts')
+@endsection
--- resources/views/new_category.blade.php
+++ resources/views/new_category.blade.php
@@ -1,71 +1,58 @@
-<?php
-	require_once("include/header.php");
-?>				
-				
-				
-				<div class="page-header">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item">Home</li>
-						<li class="breadcrumb-item active">New Category</li>
-					</ol>
+@extends('layout.app')
 
-					
+@section('title')
+Index
+@endsection
+
+@section('meta')
+@endsection
+
+@section('styles')
+@endsection
+
+@section('content')
+<div class="row justify-content-center gutters">
+	<div class="col-xl-7 col-lg-7 col-md-8 col-sm-10">
+		<form>
+			<div class="card m-0">
+				<div class="card-header">
+					<div class="card-title">Create New Category</div>
 				</div>
 				
-				<!-- Page header end -->
-				
-				<!-- Main container start -->
-				
-				<div class="main-container">
-
-					<!-- Row start -->
-					<div class="row justify-content-center gutters">
-						<div class="col-xl-7 col-lg-7 col-md-8 col-sm-10">
-							<form>
-								<div class="card m-0">
-									<div class="card-header">
-										<div class="card-title">Create New Category</div>
-									</div>
-									
-									<div class="card-body">
-										
-										
-										<div class="row gutters">
-											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-												<div class="form-group">
-												<p><code>Enter Category Name</code></p>
-													<input type="text" class="form-control" id="rtlphn" name="rtlphn" placeholder="Fruits & Vegitables" required="">
-												</div>
-											</div>
-										</div>
-										
-										<div class="form-group">
-										<p><code>Select Category Icon</code></p>
-												<div class="input-group">
-													<div class="input-group-prepend">
-														<span class="input-group-text" id="inputGroupFileAddon01">Upload Icon</span>
-													</div>
-													<div class="custom-file">
-														<input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-														<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-													</div>
-												</div>
-										</div>
-
-										
-										<button type="button" id="submit" name="submit" class="btn btn-primary float-right">Create New Category</button>
-
-									</div>
-								</div>
-							</form>
+				<div class="card-body">
+					
+					
+					<div class="row gutters">
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+							<div class="form-group">
+							<p><code>Enter Category Name</code></p>
+								<input type="text" class="form-control" id="rtlphn" name="rtlphn" placeholder="Fruits & Vegitables" required="">
+							</div>
 						</div>
 					</div>
-					<!-- Row end -->
+					
+					<div class="form-group">
+					<p><code>Select Category Icon</code></p>
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="inputGroupFileAddon01">Upload Icon</span>
+								</div>
+								<div class="custom-file">
+									<input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+									<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+								</div>
+							</div>
+					</div>
+
+					
+					<button type="button" id="submit" name="submit" class="btn btn-primary float-right">Create New Category</button>
 
 				</div>
-				<!-- Main container end -->
-	
-	
-<?php
-	require_once("include/footer.php");
-?>	
+			</div>
+		</form>
+	</div>
+</div>
+@endsection
+
+@section('scripts')
+@endsection
